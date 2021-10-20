0f88e9070e1d390885b53f7314300be29cc506f7
cristiantorresalfaro91 <cristiantorresalfaro91@gmail.com>
2021-10-19 23:19:22 -0600

    estructura diseno

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Navbar } from "./components/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Navbar />
+			<div className="container mt-5 ">
+				<div className="row">
+					<div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+						<form>
+							<div className="mb-3 row">
+								<div className="col-sm-12">
+									<input
+										type="text"
+										className="form-control"
+										placeholder="name"
+									/>
+								</div>
+							</div>
+							<div className="mb-3 row">
+								<div className="col-sm-12">
+									<input
+										type="password"
+										className="form-control"
+										id="inputPassword"
+									/>
+								</div>
+							</div>
+							<div className="mb-3 row">
+								<div type="button" className="col-sm-12">
+									<button
+										type="button"
+										className="btn btn-primary"
+									>
+										<i className="bi bi-person-plus"></i>{" "}
+										Add
+									</button>
+								</div>
+							</div>
+						</form>
+					</div>
+					<div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+						Column
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
